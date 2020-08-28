2e04adfc6591f3bc983a3e075ef4347c56638dd8
nicGardner <nicgardner@rogers.com>
2020-08-27 21:26:44 -0400

    adding frontend to repo

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  axios.get('/exercises')
+    .then(result => {
+      console.log(result.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. zoop
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
